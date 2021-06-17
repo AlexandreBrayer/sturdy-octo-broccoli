2835e9ebbe8ca94483497f0fd431a5e70aa5b1f3
NEPHIS <nephsi.alexandre@gmail.com>
2021-06-17 14:17:14 +0200

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="nav" class="navbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <router-link to="/">
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="home" class="svg-inline--fa fa-home fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"></path></svg>
+        
+          Home 
+        </router-link>
+      </li>
+      <li>
+        <router-link to="/about">
+        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="puzzle-piece" class="svg-inline--fa fa-puzzle-piece fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+          <path fill="currentColor" d="M519.442 288.651c-41.519 0-59.5 31.593-82.058 31.593C377.409 320.244 432 144 432 144s-196.288 80-196.288-3.297c0-35.827 36.288-46.25 36.288-85.985C272 19.216 243.885 0 210.539 0c-34.654 0-66.366 18.891-66.366 56.346 0 41.364 31.711 59.277 31.711 81.75C175.885 207.719 0 166.758 0 166.758v333.237s178.635 41.047 178.635-28.662c0-22.473-40-40.107-40-81.471 0-37.456 29.25-56.346 63.577-56.346 33.673 0 61.788 19.216 61.788 54.717 0 39.735-36.288 50.158-36.288 85.985 0 60.803 129.675 25.73 181.23 25.73 0 0-34.725-120.101 25.827-120.101 35.962 0 46.423 36.152 86.308 36.152C556.712 416 576 387.99 576 354.443c0-34.199-18.962-65.792-56.558-65.792z">
+          </path>
+        </svg>
+        About</router-link>
+      </li>
+    </ul>
+  </div>
+  <router-view/>
+</template>
+
+<style lang="scss">
+@import './assets/myStyle.scss';
+
+</style>
---
+++ src/components/navBar/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+    <div id="nav" class="yes">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Nav"
+}
+</script>
